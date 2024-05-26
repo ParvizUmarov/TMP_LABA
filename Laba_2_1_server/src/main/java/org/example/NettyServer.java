39f6c85bf9dea5a8fef353c7aebdcffa45ded05b
parviz <parvizumarov78@gmail.com>
2024-05-26 16:44:39 +0300

    add server

---
+++ Laba_2_1_server/src/main/java/org/example/NettyServer.java
@@ -0,0 +1,40 @@
+package org.example;
+
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class NettyServer {
+
+    public static void main(String[] args){
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG, 100)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        public void initChannel(SocketChannel ch) throws Exception {
+                              ch.pipeline().addLast(new NettyServerHandler());
+                        }
+                    });
+
+            ChannelFuture future = b.bind(Settings.PORT).sync();
+
+            future.channel().closeFuture().sync();
+        }catch (Exception e){
+            e.printStackTrace();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
